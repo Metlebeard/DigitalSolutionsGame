48eb40396c2eda87bb50aa9491fe09aa6175be5f
47161009 <Sethenglish2005@gmail.com>
2021-04-19 17:40:31 +1000

    Uploaded to GitHub

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
+
+const publicPath = path.join(__dirname, '/../public');
+const port = process.env.PORT || 2244;
+let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
+
+app.use(express.static(publicPath));
+
+io.on('connection', (socket) => {
+    console.log('A new user just connected, ' + connectedUsers + ' are here');
+
+    socket.on('disconnect', () => {
+        console.log('User was disconnected, ' + connectedUsers + ' left');
+    });
+});
+
+server.listen(port, () => {
+    console.log('Server is up on port ' + port);
+});
