6f02916ca0084c6af229424392eba2aae9e6f923
47161009 <Sethenglish2005@gmail.com>
2021-04-22 22:22:21 +1000

    Creating / Joining / Leaving rooms now works

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,242 @@
+//join server
 let socket = io();
+//declare empty room code variable
+var PIN;
 
+//add options to join or host
+loadStartScreen();
+
+//log if server is connecting
 socket.on('connect', function() {
     console.log('Connected to server');
 });
 
+//log if server is down
 socket.on('disconnect', function() {
     console.log('Disconnected from server');
+});
+
+//host a game
+function hostGame()
+{
+    socket.emit('hostRoom');
+    clearScreen();
+    loadHostScreen();
+}
+
+//join a game
+function joinGame()
+{
+    clearScreen();
+    loadJoinScreen();
+}
+
+//load first screen state
+function loadStartScreen()
+{
+    //create title
+    var title = document.createElement('h1');
+    title.textContent = "Seff's Math Game";
+    title.classList.add('title');
+    title.setAttribute('id', 'title');
+    //create host button
+    var hostButton = document.createElement('button');
+    hostButton.textContent = "HOST";
+    hostButton.classList.add('host');
+    hostButton.setAttribute('onclick', 'hostGame()');
+    hostButton.setAttribute('id', 'hostButton');
+    //create join button
+    var joinButton = document.createElement('button');
+    joinButton.textContent = "JOIN";
+    joinButton.classList.add('join');
+    joinButton.setAttribute('onclick', 'joinGame()');
+    joinButton.setAttribute('id', 'joinButton');
+    //add to body
+    document.body.appendChild(title);
+    document.body.appendChild(hostButton);
+    document.body.appendChild(joinButton);
+}
+
+//load host's screen state
+function loadHostScreen()
+{
+    //create some text
+    var text = document.createElement('p');
+    text.textContent = "Game PIN:";
+    text.setAttribute('id', 'text');
+    text.classList.add('text');
+    //create code text
+    var code = document.createElement('h1');
+    code.textContent = "";
+    code.setAttribute('id', 'code');
+    code.classList.add('code');
+    //create start button
+    var startButton = document.createElement('button');
+    startButton.textContent = "START";
+    startButton.setAttribute('id', 'startButton');
+    startButton.setAttribute('onclick', 'startGame()');
+    startButton.classList.add('startButton');
+    //create title
+    var title = document.createElement('h1');
+    title.textContent = "Seff's Math Game";
+    title.setAttribute('id', 'title');
+    title.classList.add('hostTitle');
+    //create player area
+    var playerArea = document.createElement('div');
+    playerArea.setAttribute('id', 'playerArea');
+    playerArea.classList.add('playerArea');
+    //add to body
+    document.body.appendChild(text);
+    document.body.appendChild(code);
+    document.body.appendChild(startButton);
+    document.body.appendChild(title);
+    document.body.appendChild(playerArea);
+}
+
+//load client's screen state
+function loadJoinScreen()
+{
+    //create title
+    var title = document.createElement('h1');
+    title.textContent = "Seff's Math Game";
+    title.setAttribute('id', 'title');
+    title.classList.add('joinTitle');
+    //create code input
+    var codeInput = document.createElement('input');
+    codeInput.setAttribute('placeholder', 'Enter Game PIN');
+    codeInput.setAttribute('id', 'codeInput');
+    codeInput.classList.add('codeInput');
+    //create new line
+    var newLine = document.createElement('br');
+    newLine.setAttribute('id', 'newLine');
+    //create join button
+    var joinButton = document.createElement('button');
+    joinButton.textContent = "ENTER PIN"
+    joinButton.setAttribute('id', 'joinButton');
+    joinButton.setAttribute('onclick', 'checkCode()');
+    joinButton.classList.add('joinButton');
+    //add to body
+    document.body.appendChild(title);
+    document.body.appendChild(codeInput);
+    document.body.appendChild(newLine);
+    document.body.appendChild(joinButton);
+}
+
+//add code to host's screen
+socket.on('hostCode', function(data) {
+    document.getElementById('code').textContent = data.code;
+    PIN = data.code;
+});
+
+//send code to server to check if it exists
+function checkCode()
+{
+    PIN = document.getElementById('codeInput').value;
+    socket.emit('checkCode', {
+        code: document.getElementById('codeInput').value
+    });
+}
+
+//response to checking code for existence
+socket.on('checkCodeResponse', function(response) {
+    if (response.roomFound === true)
+    {
+        clearScreen();
+        loadNameScreen();
+    }
+    else
+    {
+        alert("Code is incorrect or doesn't exist");
+    }
+});
+ 
+//allowing client to enter a custom name
+function loadNameScreen()
+{
+    //create title
+    var title = document.createElement('h1');
+    title.textContent = "Seff's Math Game";
+    title.setAttribute('id', 'title');
+    title.classList.add('joinTitle');
+    //create name input
+    var codeInput = document.createElement('input');
+    codeInput.setAttribute('placeholder', 'Enter Name');
+    codeInput.setAttribute('id', 'nameInput');
+    codeInput.classList.add('codeInput');
+    //create new line
+    var newLine = document.createElement('br');
+    newLine.setAttribute('id', 'newLine');
+    //create join button
+    var joinButton = document.createElement('button');
+    joinButton.textContent = "JOIN"
+    joinButton.setAttribute('id', 'joinButton');
+    joinButton.setAttribute('onclick', 'enterName()');
+    joinButton.classList.add('joinButton');
+    //add to body
+    document.body.appendChild(title);
+    document.body.appendChild(codeInput);
+    document.body.appendChild(newLine);
+    document.body.appendChild(joinButton);
+}
+
+//add player to host's screen when connected
+socket.on('playerJoined', function(name) {
+    var playerArea = document.getElementById('playerArea');
+    var player = document.createElement('button');
+    player.classList.add('lobbyPlayer');
+    player.textContent = name.name;
+    player.setAttribute('onclick', 'kick("' + name.name + '")');
+    player.setAttribute('id', name.name);
+    playerArea.appendChild(player);
+});
+
+//send name to server
+function enterName()
+{ 
+    var name = document.getElementById('nameInput').value;
+    if (name === '')
+        return;
+    console.log('Joining room ' + PIN + ' with the name ' + name);
+    socket.emit('joinRoom', {
+        name: name,
+        code: PIN
+    });
+}
+
+socket.on('nameError', function() {
+    alert('There is an error with your name');
+});
+
+socket.on('nameSuccessful', function() {
+    clearScreen();
+});
+
+//clear everything but the scripts
+function clearScreen()
+{
+    var num = document.body.childNodes.length
+    for (var i = 4; i < num; i++)
+    {
+        document.body.removeChild(document.body.lastChild);
+    }
+}
+
+function kick(player)
+{
+    document.getElementById(player).remove();
+    socket.emit('kickPlayer', {
+        name: player,
+        code: PIN
+    });
+}
+
+socket.on('kicked', function() {
+    console.log('kicked');
+    PIN = "";
+    clearScreen();
+    loadStartScreen();
+});
+
+socket.on('playerLeave', function(data) {
+    document.getElementById(data.name).remove();
 });
--- server/server.js
+++ server/server.js
@@ -1,24 +1,177 @@
+//include external libraries
 const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+//link public folder
 const publicPath = path.join(__dirname, '/../public');
+//set port to 2244
 const port = process.env.PORT || 2244;
+//create server functionality
 let app = express();
+//create server
 let server = http.createServer(app);
+//allow client connections
 let io = socketIO(server);
 
+//rooms
+//[0]Host Socket, [1]Room Code, [2]Player Arrays
+let rooms = [];
+
+//redirect clients to index.html in public/
 app.use(express.static(publicPath));
 
+//client connects
 io.on('connection', (socket) => {
+    //log user connections
     console.log('A new user just connected');
 
+    //if client selects "Host" add the room to the rooms array and set a keycode
+    socket.on('hostRoom', () => {
+        var room = [];
+        room.push(socket);
+
+        var roomCode = '';
+        for (var i = 0; i < 6; i++)
+        {
+            roomCode += randInt(0, 9);
+        }
+        room.push(roomCode);
+
+        var players = [];
+        room.push(players);
+
+        rooms.push(room);
+
+        console.log('room created');
+        socket.emit('hostCode', {
+            code: roomCode
+        });
+        console.log('code sent')
+    });    
+
+    //when client tries to join room see if room exists
+    socket.on('checkCode', (code) => {
+        var roomFound = false;
+        for (var i = 0; i < rooms.length; i++)
+        {
+            if (!roomFound)
+            {
+                if (code.code === rooms[i][1])
+                {
+                    roomFound = true;
+                }
+            }
+        }
+        if (roomFound)
+        {
+            socket.emit('checkCodeResponse', {
+                roomFound: true
+            });
+        }
+        else
+        {
+            socket.emit('checkCodeResponse', {
+                roomFound: false
+            });
+        }
+    });
+
+    //add players name and socket to the room with code they've used
+    socket.on('joinRoom', (data) => {
+        //player[name, points, lives, socket]
+        var player = [];
+        player.push(data.name);
+        player.push(0);
+        player.push(3);
+        player.push(socket);
+        for (var i = 0; i < rooms.length; i++)
+        {
+            if (data.code === rooms[i][1])
+            {
+
+                for (var j = 0; j < rooms[i][2].length; j++)
+                {
+                    if (data.name === rooms[i][2][j][0])
+                    {
+                        console.log('player tried to join with the same name')
+                        socket.emit('nameError');
+                        return;
+                    }
+                }
+                socket.emit('nameSuccessful');
+
+                rooms[i][2].push(player);
+                console.log('player ' + data.name + ' has joined a room');
+
+                rooms[i][0].emit('playerJoined', {
+                    name: data.name
+                });
+            }
+        }
+    });
+
+    socket.on('kickPlayer', (player) => {
+        kickPlayer(player.name, player.code);
+    });
+
+    //client disconnects
     socket.on('disconnect', () => {
         console.log('User was disconnected');
+        for (var i = 0; i < rooms.length; i++)
+        {
+            //if player is in a room leave it
+            for (var j = 0; j < rooms[i][2].length; j++)
+            {
+                if (rooms[i][2][j][3] === socket)
+                {
+                    rooms[i][0].emit('playerLeave', {
+                        name: rooms[i][2][j][0]
+                    });
+                    rooms[i][2].splice(j, 1);
+                }
+            }
+            //if player leaving is a host, delete room
+            if (rooms[i][0] === socket)
+            {
+                //kick all players connected
+                for (var j = 0; j < rooms[i][2].length; j++)
+                {
+                    kickPlayer(rooms[i][2][j][0], rooms[i][1]);
+                }
+                console.log('deleted room');
+                rooms.splice(i, 1);
+            }
+        }
     });
 });
 
+//open server on port (port is set at the top of the file)
 server.listen(port, () => {
     console.log('Server is up on port ' + port);
 });
+
+//random number generator
+function randInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function kickPlayer(name, code)
+{
+    for (var i = 0; i < rooms.length; i++)
+    {
+        if (rooms[i][1] === code)
+        {
+            for (var j = 0; j < rooms[i][2].length; j++)
+            {
+                if (rooms[i][2][j][0] === name)
+                {
+                    rooms[i][2][j][3].emit('kicked');
+                    console.log('user ' + name + ' was kicked from room ' + code);
+                    rooms[i][2].splice(j, 1);
+                }
+            }
+        }
+    }
+}
